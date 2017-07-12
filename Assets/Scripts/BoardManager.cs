3d9c555f51b139620e4c76ffc6cef9424167ccdc
Roshan Munjal <roshan.munjal@hotmail.com>
2017-07-12 12:00:12 -0400

    Initial Commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int minimum, int maximum)
+        {
+            this.minimum = minimum;
+            this.maximum = maximum;
+        }
+    }
+
+    // Variables to store dimensions of board.
+    public int columns = 8;
+    public int rows = 8;
+
+    // Variables to store the number of walls and the number of food items (randomnly generated).
+    public Count innerWallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+
+    // Variables to hold game objects.
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] innerWallTiles;
+    public GameObject[] outerWallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    private void InitializeList()
+    {
+        gridPositions.Clear();
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+
+    }
+
+    private void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+                }
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    private Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+    }
+
+    private void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        int obstacleCount = Random.Range(minimum, maximum + 1);
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+    public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitializeList();
+        LayoutObjectAtRandom(innerWallTiles, innerWallCount.minimum, innerWallCount.maximum);
+        LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+        int enemyCount = (int)Mathf.Log(level, 2f);
+        LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    public Sprite damageSprite;
+    public int hitPoints = 4;
+    public AudioClip chopSound1;
+    public AudioClip chopSound2;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        SoundManager.instance.RandomizeSfx(chopSound1, chopSound2);
+        spriteRenderer.sprite = damageSprite;
+        hitPoints -= loss;
+        if (hitPoints <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
